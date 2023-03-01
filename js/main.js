bf6f7a0c168caf8bc7a09a582514223b0c7f1d80
sdon <sdonchenko@outlook.com>
2023-03-01 18:40:19 +0200

    small fix and add hometask

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 let StudentsInfo = {}
 const arrOfStudents = [];
-function drawStudent(){
-    
-    for (const key of StudentsInfo) {
-       arrOfStudents.push(StudentsInfo[key]); 
-       console.log("hello")
+function drawStudent() {
+// поконсолить что лужит в StudentsInfo добавив несколько стундентов
+    for (const key in StudentsInfo) {
+        arrOfStudents.push(StudentsInfo[key]);
+        console.log("hello")
     }
-    console.log(arrOfStudents)
+    // по консолить что мы тут сделали сверху что тут arrOfStudents лежит
     const div = document.createElement("div");
-    // div.textContent = `Имя:${Name} Фамилия:${surname} Дата рождения: ${birth}`;
-    document.querySelector("body").append(div) 
+    arrOfStudents.forEach((el) => 
+    // вот тут ты перебираешь массив с обьектами ( твоими студентами ) и генерируешь СПИСОК в лишке один студент
+    ))
+    // div.textContent = `Имя:${Name} Фамилия:${surname} Дата рождения: ${birth}`; 
+    document.querySelector("body").append(div)
 }
 function generateStudent() {
-    console.log("lol")
 
     const StudentName = document.querySelector("#name")
     const StudentSurname = document.querySelector("#surname")
@@ -20,13 +22,14 @@ function generateStudent() {
     const birthdate = birthdateInput.value;
 
     const student = {
-        firstName:  StudentName.value,
+        firstName: StudentName.value,
         lastName: StudentSurname.value,
         birthdate: birthdate,
-        
-}
+
+    }
+    StudentsInfo[`${StudentName.value}-Student`] = student
+
     drawStudent();
-StudentsInfo[`${StudentName.value}-Student`] = student
 
 }
 document.querySelector(".btn").addEventListener("click", generateStudent);
